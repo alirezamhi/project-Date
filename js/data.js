e6995d056d42b3900e1da31521c8d32899ca00bf
alireza mohammadkhani <alirezamohammadkhani2@gmail.com>
2021-12-20 13:25:43 +0330

    add code of date

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
 let time = document.getElementById('clock');
 let date = document.getElementById('data');
 setInterval(update , 1000);
+
 function update(){
     let now = new Date();
     let second = now.getSeconds();
@@ -17,4 +18,22 @@ function update(){
     }
     let timestring = `${hour}:${minute}:${second}`;
     time.innerHTML = timestring;
+    // date.innerHTML = now.toLocaleDateString('fa-IR');
+
+    // date.innerHTML = now.toLocaleDateString('fa-IR' , {
+    //     weekday:'long',
+    //     day:'numeric', 
+    //     year:'numeric',
+    //     month:'long'
+    // })
+
+    let weekday = now.toLocaleDateString('fa-IR' , {weekday: 'long'});
+    let mothDay = now.toLocaleDateString('fa-IR' , {day: 'numeric'});
+    let year = now.toLocaleDateString('fa-IR' , {year: 'numeric'});
+    let monthName = now.toLocaleDateString('fa-IR' , {month: 'long'});
+
+    let dateString = `${weekday} , ${mothDay} ${monthName} ${year}`;
+    date.innerHTML = dateString;
 }
+
+
